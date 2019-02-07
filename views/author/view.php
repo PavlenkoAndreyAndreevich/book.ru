dfdbb299713d648ec7ec5ee892c38b6b3f633775
Andrey Pavlenko <malahan1990mu@gmail.com>
2019-02-07 21:52:15 +0200

    version 2.0

--- views/author/view.php
+++ views/author/view.php
@@ -5,4 +5,4 @@ use yii\helpers\Url;
 ?>
 <p>Автор: <?php echo $author->name; ?></p>
 <p>Количество книг: <?php echo $author->getCountBooks(); ?></p>
-<a href="<?php echo Url::to(['/author/update', 'id' => $author->id]) ?>" class="btn btn-primary">Редактировать</a>
+<a href="<?php echo Url::to(['/author/update', 'id' => $author->id]) ?>" class="btn btn-primary">Редактировать</a> 7
